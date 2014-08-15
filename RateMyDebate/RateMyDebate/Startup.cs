5e2e878c78ff88ec15ef4ae853fd8365171c2ab5
Rasmus Højte <rasmusvh@msn.com>
2014-08-15 15:17:40 +0200

    Sync project

---
+++ RateMyDebate/RateMyDebate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RateMyDebate.Startup))]
+namespace RateMyDebate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
