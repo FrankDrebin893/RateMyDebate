696e85895fcb65dec7742713e6495d2c80e999ad
nicaa <nica1408@gmail.com>
2014-08-21 20:05:08 +0200

    Context

---
+++ RateMyDebate/RateMyDebate/Models/RateMyDebateContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RateMyDebate.Models
+{
+    public class RateMyDebateContext :DbContext
+    {
+        public DbSet<UserModel> UserModel { get; set; }
+    }
+}
---
+++ RateMyDebate/RateMyDebate/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RateMyDebate.Models
+{
+    public class UserModel
+    {
+        public int accountId { get; set; }
+        public String userName { get; set; }
+        public String Password { get; set; }
+
+    }
+}
