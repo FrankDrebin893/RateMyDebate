850efd0aff8c4bc1f117f64daea06ed876ffb039
nicaa <nica1408@gmail.com>
2014-08-23 17:04:15 +0200

    dbfix

--- RateMyDebate/RateMyDebate/App_Start/RouteConfig.cs
+++ RateMyDebate/RateMyDebate/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RateMyDebate
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "RasmusIndex", id = UrlParameter.Optional }
             );
         }
     }
