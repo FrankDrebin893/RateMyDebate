60880b80cd54c3e9c196c9df4ce28b5b4ce33ce9
nicaa <nica1408@gmail.com>
2014-08-25 14:40:35 +0200

    DebateFAQDesign

---
+++ RateMyDebate/RateMyDebate/Controllers/FAQController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RateMyDebate.Controllers
+{
+    public class FAQController : Controller
+    {
+        //
+        // GET: /FAQ/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult WhatIsRateMyDebate()
+        {
+            return View();
+        }
+
+	}
+}
