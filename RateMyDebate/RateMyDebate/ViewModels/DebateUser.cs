a9f3bf61dc604df69159eb6432a9e44c3d272139
Rasmus Højte <rasmusvh@msn.com>
2014-08-25 14:39:46 +0200

    DebateINdexDesign

--- RateMyDebate/RateMyDebate/ViewModels/DebateUser.cs
+++ RateMyDebate/RateMyDebate/ViewModels/DebateUser.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using RateMyDebate.Models;
 
 namespace RateMyDebate.ViewModels
 {
     public class DebateUser
     {
+       
+        public List<UserInformation> User { get; set; }
+        public List<Debate> Debate { get; set; }
+
+        public List<Category> Categories { get; set; } 
+
 
     }
 }
