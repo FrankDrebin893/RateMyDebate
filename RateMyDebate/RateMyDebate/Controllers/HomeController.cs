bf0d2884c4c68fb072a10310e3c437ce3046c348
nicaa <nica1408@gmail.com>
2014-09-09 12:27:35 +0200

    user auth

--- RateMyDebate/RateMyDebate/Controllers/HomeController.cs
+++ RateMyDebate/RateMyDebate/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using RateMyDebate.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace RateMyDebate.Controllers
 {
     public class HomeController : Controller
     {
+        //private RateMyDebateContext db = new RateMyDebateContext();
         public ActionResult Index()
         {
             //YO
@@ -35,6 +38,55 @@ namespace RateMyDebate.Controllers
         public ActionResult RasmusIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                if(IsValid(user.userName, user.Password)){
+
+                FormsAuthentication.SetAuthCookie(user.userName, false);
+                return RedirectToAction("RasmusIndex", "Home");
+                }
+                  else
+                {
+                ModelState.AddModelError("", "Logins is wrong");
+                }
+            }
+            return View(user);
+        }
+
+        private bool IsValid(string userName, string password)
+        {
+            bool isValid = false;
+            using (var db = new RateMyDebateContext())
+            {
+                var user = db.UserModel.FirstOrDefault(u => u.userName == userName);
+                if(user != null){
+                    if (user.Password == password)
+                    {
+                        isValid = true;
+                    }
+                }
+               
+            }
+            
+
+
+            return isValid;
+        }
+        public ActionResult LogOut(){
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("RasmusIndex", "Home");
         }
     }
 }
